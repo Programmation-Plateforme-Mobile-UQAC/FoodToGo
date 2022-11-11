682da51d7f644ebb7cbaddbd9402326c2e63d422
AsareelDadiouari <asareel56@gmail.com>
2022-11-11 02:22:26 -0500

    Feat: Added Product RecycleView & Product Card Fragment

--- app/src/main/java/com/example/foodtogo/ui/product/AddProductActivity.java
+++ app/src/main/java/com/example/foodtogo/ui/product/AddProductActivity.java
@@ -2,23 +2,37 @@ package com.example.foodtogo.ui.product;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 
 import com.example.foodtogo.R;
-import com.example.foodtogo.databinding.ActivityAddProductBinding;
 
 import org.jetbrains.annotations.Nullable;
 
 public class AddProductActivity extends AppCompatActivity  {
-    ActivityAddProductBinding binding;
+    ImageButton imageButton1, imageButton2, imageButton3;
+    EditText foodTypeEdit, namePostEdit, productDescriptionEdit, expirationDateEdit;
+    Button addPostButton;
 
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityAddProductBinding.inflate(getLayoutInflater());
-        View view = binding.getRoot();
-        this.setContentView(view);
+        this.setContentView(R.layout.activity_add_product);
+
+        bindVariables();
+
+    }
+
+    private void bindVariables(){
+        imageButton1 = findViewById(R.id.imageButton1);
+        imageButton2 = findViewById(R.id.imageButton2);
+        imageButton3 = findViewById(R.id.imageButton3);
+
+        foodTypeEdit = findViewById(R.id.foodTypeEdit);
+        namePostEdit = findViewById(R.id.namePostEdit);
+        productDescriptionEdit = findViewById(R.id.productDescriptionEdit);
+        expirationDateEdit = findViewById(R.id.expirationDateEdit);
+
+        addPostButton = findViewById(R.id.addPostButton);
     }
 }
