40842e930d5820681d4670dd21ecbe75b57cbff4
AsareelDadiouari <asareel56@gmail.com>
2022-11-11 00:02:31 -0500

    Feat: Update Product & Added AddProduct & MainActivity

--- app/src/main/java/com/example/foodtogo/MainActivity.java
+++ app/src/main/java/com/example/foodtogo/MainActivity.java
@@ -9,6 +9,6 @@ import org.jetbrains.annotations.Nullable;
 public class MainActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        this.setContentView(R.layout.activity_login);
+        this.setContentView(R.layout.activity_main);
     }
 }
